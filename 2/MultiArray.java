4a4610e5455472478af7c332ef4604ba72ccddc3
yuusari <yuusari@gmail.com>
2023-12-19 20:15:33 +0900

    1

--- 2/MultiArray.java
+++ 2/MultiArray.java
@@ -1,13 +1,16 @@
 class MultiArray{
 
     public static void main(String[] args ){
-        int [][] allScore = new int[2] [3] ;
+        /*int [][] allScore = new int[2] [3] ;
         allScore[0][0] = 80;
         allScore[0][1] = 100;
         allScore[0][2] = 75;
         allScore[1][0] = 85;
         allScore[1][1] = 95;
-        allScore[1][2] = 80;
+        allScore[1][2] = 80; */
+
+
+        int[][] allScore = {{10,20,30},{40,50,60}};
 
         System.out.println(allScore[0][0] + "“_");
         System.out.println(allScore[0][1] + "“_");
